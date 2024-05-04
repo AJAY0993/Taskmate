c642f125d9d42417030ae6668cfda22b26105bbe
AJAY0993 <ajaymishra5277919886@gmail.com>
2024-05-05 01:24:03 +0530

    added suspense fallback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
 import AuthCheck from "./components/AuthCheck";
 import store from "./store";
 import Modal from "./components/Modal";
+import FullPageLoader from "./components/FullPageLoader";
 
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
@@ -17,22 +18,24 @@ const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <AuthCheck>
-          <Modal>
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/dashboard" element={<DashBoard />} />
-              <Route path="/dashboard/:id" element={<Board />} />
-              <Route path="/about" element={<AboutMe />} />
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
-          </Modal>
-        </AuthCheck>
-      </BrowserRouter>
+      <Suspense fallback={<FullPageLoader />}>
+        <BrowserRouter>
+          <AuthCheck>
+            <Modal>
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/dashboard" element={<DashBoard />} />
+                <Route path="/dashboard/:id" element={<Board />} />
+                <Route path="/about" element={<AboutMe />} />
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Modal>
+          </AuthCheck>
+        </BrowserRouter>
+      </Suspense>
       <Toaster />
     </Provider>
   );
